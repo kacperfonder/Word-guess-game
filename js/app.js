4e6029b0b1d4e9523b4d959a460f2d63601c5188
kacperfonder <kacper.fonder@gmail.com>
2018-03-27 20:09:10 +0200

    game variables and elements

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const game = {
+
+
+    lives: 10,
+    currSentence: '',
+    currLetter: '',
+    sentence: ['Im a Pickle', 'password', 'herkules', 'terminator'],
+    alphabet: ['a','b','c','d','e','f','g','h','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+
+    eleSentence: document.querySelector('.sentence'),
+    eleLives: document.querySelector('.lives'),
+    eleLetters: document.querySelector('.letters'),
+    
+    
+}
+
+
