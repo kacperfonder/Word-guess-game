65ea25257544d6095df8a0b3028fe740b9570474
kacperfonder <kacper.fonder@gmail.com>
2018-03-28 14:55:10 +0200

    show lives function

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const game = {
     eleLetters: document.querySelector('.letters'),
  
     generateButtons () {
-        const alphabet = ['a','b','c','d','e','f','g','h','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+        const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     
         let self = this;
         alphabet.forEach(letter => {
@@ -18,18 +18,21 @@ const game = {
             button.classList.add('letter');
             button.type = 'button'
             button.innerHTML = letter;
-            // button.dataset.letter = letter;
             self.eleLetters.appendChild(button)
         });
     },
 
     btnsOnClick () {
-        this.eleLetters.addEventListener('click', function(e) {
-                // const letter = e.target.dataset.letter;
+        this.eleLetters.addEventListener('click', function(e) {        
                 const letter = e.target.innerHTML
                 e.target.disabled = true;
                 console.log(letter);
         });
+    },
+    showLives () {
+        this.eleLives.innerHTML = this.lives;
+       
+         
     },
     
     startGameBtn () {
