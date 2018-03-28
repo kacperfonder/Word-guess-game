1aa1cfd4deb71aeca6e1e5d22229196b0c25958c
kacperfonder <kacper.fonder@gmail.com>
2018-03-28 23:35:50 +0200

    add check letter func

--- js/app.js
+++ js/app.js
@@ -2,16 +2,14 @@ const game = {
     lives: 8,
     currSentence: '',
     currSplitedSentense: '',
-    sentences: ['Pickle','password','herkules','terminator'],
+    sentences: ['pickle','password','herkules','terminator','andrew','monkey',],
    
-
     eleSentence: document.querySelector('.sentence'),
     eleLives: document.querySelector('.lives'),
     eleLetters: document.querySelector('.letters'),
  
     generateButtons () {
         const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    
         let self = this;
         alphabet.forEach(letter => {
             const button = document.createElement('button');
@@ -21,26 +19,44 @@ const game = {
             self.eleLetters.appendChild(button)
         });
     },
-
+    
     btnsOnClick () {
-        this.eleLetters.addEventListener('click', function(e) {        
-                const letter = e.target.innerHTML
-                e.target.disabled = true;
-                console.log(letter);
+        let self = this    
+
+        this.eleLetters.addEventListener('click', function(e) {  
+            const letter = e.target.innerHTML
+            self.checkLetter(letter);
+            e.target.disabled = true;
+            console.log(letter);
         });
     },
+
+    checkLetter (e) { 
+        if (this.currSentence.indexOf(e) !== -1) { 
+            for (let i=0; i<this.currSentence.length; i++) {
+                if (this.currSentence[i] === e) {
+                    this.eleSentence.querySelectorAll('.letter-box')[i].innerHTML = e; 
+
+                }
+            }
+        }
+    },
+  
+
     showLives () {
         this.eleLives.innerHTML = this.lives;
-         
     },
+
     randomSentence () {
         this.currSentence = this.sentences[Math.floor(Math.random()*this.sentences.length)];
         console.log(this.currSentence);
-        
-        this.currSplitedSentense = this.currSentence.split('');
+
+        this.currSplitedSentense = this.currSentence.split('')
         console.log(this.currSplitedSentense);
         
-        let splitedWord = this.currSplitedSentense;
+        this.eleSentence.innerHTML = '';
+        
+        let splitedWord = this.currSplitedSentense
         splitedWord.forEach(e => {
             const box = document.createElement('div');
             box.classList.add('letter-box');
@@ -62,6 +78,7 @@ const game = {
         this.btnsOnClick();
     }
 };
+
     game.gameBoard();
 
 document.querySelector('.game-start-btn').addEventListener('click', function() {
